d2afcb494ea37a9e2359fda3f0074867816a87ff
alizaman15g-coder <alizaman15g@gmail.com>
2026-05-01 17:29:03 +0500

    Add files via upload

---
+++ WEEK-9/Task-1.py
@@ -0,0 +1,87 @@
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(i):
+#         print(j,end="")
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(i+1):
+#         print(j,end="")
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()    
+
+
+# for i in range(1,5):
+#     for j in range(0,i+1):
+#          print(j,end="")
+#     print()    
+
+
+# for i in range(1,5):
+#     for j in range(-1,i+1):
+#          print(j,end="")
+#     print()   
+
+      
+# for i in range(1,5):
+#          for j in range(1,5):
+#           print(i*j,end="\t")
+#          print()
+
+# for i in range(1,11):
+#          for j in range(1,5):
+#           print(i*j,end="\t")
+#          print()
+
+# for i in range(1,5):
+#          for j in range(1,11):
+#           print(i*j,end="\t")
+#          print()
+
+# for i in range(1,11):
+#          for j in range(1,11):
+#           print(i*j,end="\t")
+#          print()
+
+
+# for i in range(1,5):
+#         for j in range(4-i):
+#          print("",end="")
+#         print()
+
+
+# for i in range(1,5):
+#      for j in range(4-i):
+#          print(" ",end="")
+#      for i in range(2*i-1):
+#                print("*",end="")
+   
+#      print()
+
+# for i in range(1,5):
+#     for j in range(4-i):
+#         print("*",end="")
+#     for i in range(2*i-1):
+#               print("*",end="")
+   
+#     print()
+
+
+for i in range(1,5):
+     for j in range(4-i):
+         print(" ",end="")
+     for i in range(2*i-1):
+               print("Y",end="")
+   
+     print()
